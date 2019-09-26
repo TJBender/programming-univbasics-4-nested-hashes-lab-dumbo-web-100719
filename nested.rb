c1ab5c68cdc76cb23aadebb52a0acb0502f51807
Tiana Bender <tiana.bender01@gmail.com>
2019-09-26 19:33:27 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,11 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+programmer_hash[0] = {
+  :yukihiro_matsumoto => {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 	programmer_hash = 
  		{
         :grace_hopper => {
